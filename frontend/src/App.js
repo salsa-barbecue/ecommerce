5e9f1ad0a6ae00724d391aa5b9d835386b64f7d3
Luca Leone <lucaleone.business@gmail.com>
2024-02-11 16:40:50 +0100

    initial frontend commit

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="text-3xl font-bold underline">
+          Test
+        </h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
